fc07babba47ad78ca0b110dfd2cce4bb76a8e47d
Amar Amar <amarjedoumaman@gmail.com>
2018-03-12 21:15:19 +0000

    My Project setup completed successfully

---
+++ shoppingbackend/src/main/java/com/amar/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.amar.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
