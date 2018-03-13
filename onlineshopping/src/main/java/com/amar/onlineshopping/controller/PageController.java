7aede09ccba48e5deaab1324b0d90eca28ac2dc3
Amar Amar <amarjedoumaman@gmail.com>
2018-03-13 21:53:39 +0000

    Successfully Loaded static resources

---
+++ onlineshopping/src/main/java/com/amar/onlineshopping/controller/PageController.java
@@ -0,0 +1,46 @@
+package com.amar.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value = {"/" ,"/home" , "/index"})
+	public ModelAndView index(){
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to Web MVC");
+		return mv;
+	}
+	
+	
+	/*@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value="greeting" , required=false)String greeting){
+		
+		if(greeting == null){
+			 greeting ="hello";
+		}
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}*/
+	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting")String greeting){
+		
+		if(greeting == null){
+			 greeting ="hello";
+		}
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
+	
+
+}
